9c27c934da2df856a82973beb3e0ee0839d97b04
liangwl <liangwl@sunhotin.com>
2023-01-24 19:54:24 +0800

    ci(@willy/utils): 创建项目的通用工具包

---
+++ packages/willy-utils/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @ Author: willysliang
+ * @ Create Time: 2023-01-24 17:48:56
+ * @ Modified by: willysliang
+ * @ Modified time: 2023-01-24 17:49:07
+ * @ Description: 通用工具包
+ */
+
---
+++ packages/willy-utils/utils/http/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @ Author: willysliang
+ * @ Create Time: 2023-01-24 17:50:27
+ * @ Modified by: willysliang
+ * @ Modified time: 2023-01-24 18:29:35
+ * @ Description: 网络请求
+ */
